869e025d22882b2bf3ceb85161014b99ecf00937
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-04-26 14:30:02 +0400

    Done with product-detail

--- client/assets/js/product-detail.js
+++ client/assets/js/product-detail.js
@@ -4,7 +4,7 @@ const shippingAccordion = document.querySelector(".shipping-return .top");
 const shippingAccordionText = document.querySelector(
   ".shipping-return .bottom"
 );
-
+const variationName = document.querySelector(".variation-name span");
 const variants = document.querySelectorAll(".variations label");
 
 const slideCount = document.querySelectorAll(
@@ -31,6 +31,8 @@ shippingAccordion.addEventListener("click", () => {
 });
 
 // Change variations
+variationName.textContent = variants[0].textContent;
+
 variants.forEach((variant) => {
   variant.addEventListener("click", () => {
     if (variant.classList.contains("out")) return;
@@ -39,6 +41,7 @@ variants.forEach((variant) => {
       label.classList.remove("active");
     });
 
+    variationName.textContent = variant.textContent;
     variant.classList.add("active");
   });
 });
@@ -54,17 +57,30 @@ const moveSlide = (event) => {
 
   if (button === backBtn || button === mobileBackBtn) {
     if (sliderIndex > 0) sliderIndex--;
-    else sliderIndex = slideCount - 4;
+    else if (button === backBtn) sliderIndex = slideCount - 4;
+    else if (button === mobileBackBtn) sliderIndex = slideCount - 2;
   } else if (button === frwrdBtn || button === mobileFrwrdBtn) {
-    if (sliderIndex < slideCount - 4) sliderIndex++;
+    if (sliderIndex < slideCount - 4 && button === frwrdBtn) sliderIndex++;
+    else if (sliderIndex < slideCount - 2 && button === mobileFrwrdBtn)
+      sliderIndex++;
     else sliderIndex = 0;
   }
 
+  let slideWidth = 0;
+
+  slides.forEach((slide) => {
+    if (!slide.classList.contains("active")) slideWidth = slide.clientWidth;
+  });
+
+  mobileSlideCount.textContent = `${sliderIndex + 1} / ${slideCount}`;
+
   imgContainer.style.transform = `translateX(${-(
-    slides[0].clientWidth * sliderIndex +
+    slideWidth * sliderIndex +
     12 * sliderIndex
   )}px)`;
 };
+
+mobileSlideCount.textContent = `1 / ${slideCount}`;
 
 backBtn.addEventListener("click", (event) => moveSlide(event));
 frwrdBtn.addEventListener("click", (event) => moveSlide(event));
@@ -101,7 +117,6 @@ if (windowWidth > 425) {
   };
 
   // Change main image src on slide click and add "active" to slide
-
   slides.forEach((slide) => {
     slide.addEventListener("click", () => {
       slides.forEach((img) => {
