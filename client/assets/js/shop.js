648fa92a465e89f10ebd3a76d16319d108585b66
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-04-30 15:00:49 +0400

    Added login and register pages

--- client/assets/js/shop.js
+++ client/assets/js/shop.js
@@ -3,6 +3,9 @@
 const filters = document.querySelector(".shop-section .filters");
 const filterSortBtn = document.querySelector(".shop-section .filter-sort");
 const shopOverlay = document.querySelector(".shop-section .overlay");
+const filterCloseBtn = document.querySelector(
+  ".shop-section .filters .close-filter"
+);
 
 const filterCategoriesBtn = document.querySelector(
   "main .shop-section .filter-categories-btn"
@@ -95,15 +98,26 @@ disabledBtns.forEach((btn) => {
 shopOverlay.classList.add("hidden");
 if (windowWidth <= 768) {
   filters.classList.add("hidden");
-  filterSortBtn.addEventListener("click", () => {
-    filters.classList.remove("hidden");
-    shopOverlay.classList.remove("hidden");
-    document.body.style.overflow = "hidden";
-  });
+} else filters.classList.remove("hidden");
 
-  shopOverlay.addEventListener("click", () => {
+window.addEventListener("resize", () => {
+  if (windowWidth <= 768) {
     filters.classList.add("hidden");
+    filterSortBtn.addEventListener("click", () => {
+      filters.classList.remove("hidden");
+      shopOverlay.classList.remove("hidden");
+      document.body.style.overflow = "hidden";
+    });
+
+    [shopOverlay, filterCloseBtn].forEach((el) => {
+      el.addEventListener("click", () => {
+        filters.classList.add("hidden");
+        shopOverlay.classList.add("hidden");
+        document.body.style.overflow = "";
+      });
+    });
+  } else {
+    filters.classList.remove("hidden");
     shopOverlay.classList.add("hidden");
-    document.body.style.overflow = "";
-  });
-}
+  }
+});
