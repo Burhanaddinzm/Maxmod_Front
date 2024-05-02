9c6872c9e3fbbeb446f8e600a0e0111a3c4121c4
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-05-02 14:33:35 +0400

    Added slider functionality to each section

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -19,3 +19,82 @@ const scrollAnimation = () => {
 
 scrollAnimation();
 window.addEventListener("scroll", scrollAnimation);
+
+// Add slider functionality to each section.
+const moveSlide = (
+  event,
+  gap,
+  visibleSlideCount,
+  slideCount,
+  slideWrapper,
+  slides,
+  backBtn,
+  frwrdBtn,
+  sectionIndex // Pass section index to identify which section is being targeted
+) => {
+  if (slideCount === 1) return;
+
+  const button = event.target.closest(".slider-back, .slider-frwd");
+
+  if (!button) return;
+
+  // Get sliderIndex for the current section
+  let sliderIndex = parseInt(sections[sectionIndex].dataset.sliderIndex || "0");
+
+  if (button === backBtn) {
+    if (sliderIndex > 0) sliderIndex--;
+    else sliderIndex = slideCount - visibleSlideCount;
+  } else if (button === frwrdBtn) {
+    if (sliderIndex < slideCount - visibleSlideCount) sliderIndex++;
+    else sliderIndex = 0;
+  }
+
+  let slideWidth = slides[0].clientWidth;
+
+  slideWrapper.style.transform = `translateX(${-(
+    slideWidth * sliderIndex +
+    gap * sliderIndex
+  )}px)`;
+
+  // Update the sliderIndex for the current section
+  sections[sectionIndex].dataset.sliderIndex = sliderIndex;
+};
+
+sections.forEach((section, index) => {
+  const slider = section.querySelector(".slider");
+  if (!slider) return;
+  const slideCount = slider.querySelectorAll(".slide").length;
+  const slideWrapper = slider.querySelector(".slide-wrapper");
+  const slides = slider.querySelectorAll(".slide");
+  const backBtn = slider.querySelector(".slider-back");
+  const frwrdBtn = slider.querySelector(".slider-frwd");
+
+  if (section.classList.contains("shop-by-category-section")) {
+    backBtn.addEventListener("click", (event) =>
+      moveSlide(
+        event,
+        30,
+        6,
+        slideCount,
+        slideWrapper,
+        slides,
+        backBtn,
+        frwrdBtn,
+        index
+      )
+    );
+    frwrdBtn.addEventListener("click", (event) =>
+      moveSlide(
+        event,
+        30,
+        6,
+        slideCount,
+        slideWrapper,
+        slides,
+        backBtn,
+        frwrdBtn,
+        index
+      )
+    );
+  }
+});
